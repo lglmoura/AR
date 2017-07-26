2fc27b42911f242e69b0d380615c046b76f4d171
Luiz Gustavo <lglmoura@gmail.com>
2017-07-26 17:34:15 -0300

    Criando o AR Activity

---
+++ app/src/main/java/pooa20171/iff/br/artouristguide2/ARActivity.java
@@ -0,0 +1,13 @@
+package pooa20171.iff.br.artouristguide2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ARActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar);
+    }
+}
--- app/src/main/java/pooa20171/iff/br/artouristguide2/MainActivity.java
+++ app/src/main/java/pooa20171/iff/br/artouristguide2/MainActivity.java
@@ -1,13 +1,32 @@
 package pooa20171.iff.br.artouristguide2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btAr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btAr = (Button) findViewById(R.id.bt_ar);
+
+        btAr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ar = new Intent(getContext(),ARActivity.class);
+                startActivity(ar);
+
+            }
+        });
+    }
+    private Context getContext(){
+        return this;
     }
 }
